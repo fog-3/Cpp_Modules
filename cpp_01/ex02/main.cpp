e6814029a73e3c8c0180c9ab2eaca0d0e720857b
fernando <Fog-03@hotmail.com>
2025-09-17 20:36:46 +0200

    Uploading cpp01/ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+int main(void){
+    std::string message = "HI THIS IS BRAIN";
+    std::string *stringPTR = &message;
+    std::string &stringREF = message;
+
+    std::cout << "Memory address of the string: " << &message << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << std::endl;
+    
+    std::cout << "Value of the string: " << message << std::endl;
+    std::cout << "Value pointed to stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to stringREF: " << stringREF << std::endl;
+}
